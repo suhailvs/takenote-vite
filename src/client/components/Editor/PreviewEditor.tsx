8358619b9de60eaab4cf0f38544d579467302f2c
suhailvs <suhailvs@gmail.com>
2024-11-06 16:13:38 +0530

    closes #2: added code prettier

--- src/client/components/Editor/PreviewEditor.tsx
+++ src/client/components/Editor/PreviewEditor.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 // import ReactMarkdown from 'react-markdown'
 import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
 import { NoteItem } from '../../../client/types'
 
 import { uuidPlugin } from '../../utils/reactMarkdownPlugins'
-import 'github-markdown-css'
+import 'highlight.js/styles/github.css';
+import 'github-markdown-css';
 
 // https://github.com/remarkjs/react-markdown/issues/339#issuecomment-653396337
 // window.process = { cwd: () => '' };
@@ -23,7 +25,7 @@ export const PreviewEditor: React.FC<PreviewEditorProps> = ({ noteText, directio
   console.log(notes)
   return (
     <div className='markdown-body'>
-      <Markdown remarkPlugins={[uuidPlugin]} className={`previewer previewer_direction-${directionText}`}>
+      <Markdown remarkPlugins={[uuidPlugin]} rehypePlugins={[rehypeHighlight]} className={`previewer previewer_direction-${directionText}`}>
         {noteText}
       </Markdown>
     </div>
